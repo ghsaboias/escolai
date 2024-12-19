0d06acd01b81bd1717ace40c5974400abec88383
Brain <gsaboia@yahoo.com>
2024-12-19 14:22:53 -0300

    fix: added Button export

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -1,7 +1,9 @@
 export * from './AnimatedContent';
+export * from './Button';
 export * from './Card';
 export * from './Footer';
 export * from './Header';
 export * from './SectionTitle';
 export * from './StartCourseButton';
+
 
